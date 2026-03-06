9d2b7ce277c25d05ff465ab9e6362b2ccc156fd5
cingab1974 <gabriele@mtbfan.it>
2026-03-06 12:17:00 +0100

    Add files via upload

---
+++ comando.sh
@@ -0,0 +1,23 @@
+./frpc -c ./frpc.toml
+
+docker volume create n8n_data
+
+docker run -it --name n8n -p 5678:5678 -v n8n_data:/home/node/.n8n --env WEBHOOK_URL=https://3565.frp.darklabs.it docker.n8n.io/n8nio/n8n
+
+sudo apt install cargo (solo per installare i pacchetti altrimenti scaricare la release)
+
+https://github.com/zclod/corsoai2026
+
+https://github.com/sigoden/aichat
+
+/home/user/.config/aichat/config.yaml
+
+https://pypi.org/project/aider-chat/
+sudo apt install python3-venv python3-pip
+python -m pip install aider-install
+aider-install
+
+
+https://opencode.ai/
+
+
